f2ecbd893a94675bdc744b03636e47318537e684
twistezo <luk89@outlook.com>
2017-03-31 15:39:59 +0200

    add properties file and its behaviour

--- src/main/java/com/twistezo/SceneController.java
+++ src/main/java/com/twistezo/SceneController.java
@@ -4,15 +4,22 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Properties;
 
 
 /**
  * @author twistezo (13.03.2017)
  */
 public class SceneController {
+    private static final Logger LOG = LogManager.getLogger(SceneController.class);
+    private PropertiesManager propertiesManager = PropertiesManager.getInstance();
+    private Properties properties = propertiesManager.getProperties();
     @FXML private TextField login;
     @FXML private TextField password;
     @FXML private TextField downloadPath;
@@ -24,6 +31,8 @@ public class SceneController {
         PrintStream ps = new PrintStream(console, true);
         System.setOut(ps);
         System.setErr(ps);
+
+        afterStart();
     }
 
     @FXML
@@ -32,10 +41,45 @@ public class SceneController {
         settings.setLogin(login.getText());
         settings.setPass(password.getText());
         settings.setDownloadFolder(downloadPath.getText());
-
+        updatePropertiesFromUserInput();
         new Thread(() -> {
                 BookDownloader bookDownloader = BookDownloader.getInstance();
                 bookDownloader.startDownload();
             }).start();
+    }
+
+    private void afterStart() {
+        if (!propertiesManager.checkPropertiesFileExist()) {
+            LOG.info("File config.properties doesn't exists.");
+            LOG.info("Create default config.properties file.");
+            propertiesManager.createPropFile();
+            propertiesManager.getPropertiesFromFile();
+            setTextFieldsWithProperties();
+        } else if (propertiesManager.checkPropertiesFileExist()) {
+            LOG.info("File config.properties exists.");
+            propertiesManager.getPropertiesFromFile();
+            setTextFieldsWithProperties();
+        }
+    }
+
+    private void setTextFieldsWithProperties() {
+        login.setText(properties.getProperty(PropertiesManager.LOGIN));
+        password.setText(properties.getProperty(PropertiesManager.PASSWORD));
+        downloadPath.setText(properties.getProperty(PropertiesManager.DOWNLOAD_PATH));
+    }
+
+    private void updatePropertiesFromUserInput() {
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(PropertiesManager.PROP_FILE_NAME);
+            properties.setProperty(PropertiesManager.LOGIN, login.getText());
+            properties.setProperty(PropertiesManager.PASSWORD, password.getText());
+            properties.setProperty(PropertiesManager.DOWNLOAD_PATH, downloadPath.getText());
+            properties.store(out, null);
+            out.close();
+        } catch (IOException e) {
+            LOG.error(e.getMessage());
+        }
+
     }
 }
